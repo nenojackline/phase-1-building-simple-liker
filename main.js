615f731f8bd0805cba26348d800b374c3166af34
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-16 00:20:29 +0300

    Done with assignment

--- main.js
+++ main.js
@@ -3,6 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').className = 'hidden'
+
+const likeButtons = document.querySelectorAll('.like-glyph')
+
+function handleLikes() {
+    likeButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+
+            if (event.target.innerHTML == EMPTY_HEART) {
+                mimicServerCall("file:///Users/eliasventura/Development2/code/phase-1/phase-1-building-simple-liker/index.html")
+                    .then(function(resp) {
+
+                        event.target.innerHTML = FULL_HEART
+                        event.target.classList.add('activated-heart')
+
+                    })
+
+
+                .catch(function(error) {
+                    let modal = document.querySelector('#modal')
+                    modal.classList.remove('hidden')
+                    modal.innerText = "Random server error. Try again."
+
+                    setTimeout(() => {
+                        modal.classList.add('hidden')
+                    }, 3000);
+                })
+
+
+
+            } else {
+                event.target.innerHTML = EMPTY_HEART
+                event.target.classList.remove('activated-heart')
+            }
+        })
+    })
+
+}
+handleLikes()
+
+
+
 
 
 
